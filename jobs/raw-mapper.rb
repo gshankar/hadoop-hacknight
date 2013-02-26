423b789efa85b1e6feb58cbac7971e4700d0b906
Ganesh Shankar <ganesh@ganeshshankar.com>
2013-02-26 19:31:16 +1100

    more stuff

--- jobs/raw-mapper.rb
+++ jobs/raw-mapper.rb
@@ -1,22 +1,27 @@
 def go
   ARGF.each do |line|
     line = line.chomp
-    fields = line.split("\n")
-    for token in tokenize(fields[0])
-      puts "#{token}\t1" if acceptable?(token)
+    words = line.split(" ")
+    # fields = line.split("\n")
+    # for token in tokenize(line)
+    #   puts "#{token}\t1" if acceptable?(token)
+    # end
+    word.each do |word|
+      puts "#{word}\t1" if word.downcase == "thou"
     end
+
   end
 end
 
-def tokenize string
-  string.split(/\s+/).map do |token|
-    # stripped = token.gsub(/[^a-zA-Z0-9\']+/, '')
-    stripped.downcase
-  end
-end
+# def tokenize string
+#   string.split(/\s+/).map do |token|
+#     # stripped = token.gsub(/[^a-zA-Z0-9\']+/, '')
+#     stripped.downcase
+#   end
+# end
 
-def acceptable? token
-  (3..100).include?(token.length)
-end
+# def acceptable? token
+#   (3..100).include?(token.length)
+# end
 
 go
