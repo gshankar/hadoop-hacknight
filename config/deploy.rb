5cd48deaed4b387b9d33d8265f29056c400b1e45
Andrew Harvey <andrew@mootpointer.com>
2013-02-26 17:28:24 +1100

    Fix bundle install

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,13 @@
+require 'capistrano-rbenv'
+
 set :application, "hadoop-hacknight"
 set :repository,  "git@github.com:mootpointer/hadoop-hacknight.git"
 set :user, "user"
 set :ssh_options, { :forward_agent => true }
+
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
 
 set :deploy_via, :remote_cache
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -18,10 +24,24 @@ namespace :deploy do
   task :restart do ; end
 end
 
+namespace :hadoop do
+  task :go do
+    run "cd #{current_path} && hadoop jar /opt/cloudera/parcels/CDH-4.1.3-1.cdh4.1.3.p0.23/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-*streaming*.jar 	\
+  -D mapred.job.name='#{job}-#{input}-#{output}' \
+  -files        'jobs/#{job}-mapper.rb' \
+  -files        'jobs/#{job}-reducer.rb' \
+  -mapper       'ruby #{job}-mapper.rb' \
+  -reducer      'ruby #{job}-reducer.rb' \
+  -input        'hdfs:///user/user/#{input}' \
+  -output       'hdfs:///user/user/#{output}' \
+  -cmdenv       'LANG=en_AU.UTF-8'"
+  end
+end
+
 
 namespace :wukong do
   task :go do
-    run "cd #{current_path} && bundle exec wu-hadoop #{current_path}/jobs/#{job}.rb --mode=hadoop -input=#{input}\
+    run "cd #{current_path} && wu-hadoop #{current_path}/jobs/#{job}.rb --mode=hadoop --input=#{input}\
     --output=#{output} --hadoop_runner=/usr/bin/hadoop\
     --hadoop_home=/opt/cloudera/parcels/CDH-4.1.3-1.cdh4.1.3.p0.23/lib/hadoop-0.20-mapreduce"
   end
